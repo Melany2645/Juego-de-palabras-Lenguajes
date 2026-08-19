84bda2f4995c24590f4ad3de595aef63ad3e07ba
Melany Jirón Díaz <m.jiron.2@estudiantec.cr>
2026-08-18 18:22:58 -0600

    Instalación de dependencias, como nodemon

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(
+    "Vamos empezando con el juego de palabras de Lenguajes de Programación",
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
