ea6e23cbe14c599fb18f41309428c47e8d149200
Melany Jirón Díaz <m.jiron.2@estudiantec.cr>
2026-08-18 23:52:46 -0600

    Funciones para poder manipular la información del JSON, donde se encuentra los datos de las partidas y jugadores

---
+++ backend/manipulacionData.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const path = require("path");
+
+const ArchivoPartidas = path.join(__dirname, "data", "partidas.json");
+
+// Así podremos leer el archivo JSON y devolver su contenido como un objeto
+function leerArchivo() {
+  try {
+    const data = fs.readFileSync(ArchivoPartidas, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error al leer el archivo:", error);
+    return [];
+  }
+}
+
+// Guardamos los datos en el archivo JSON
+function guardarArchivo(partidas) {
+  try {
+    fs.writeFileSync(
+      ArchivoPartidas,
+      JSON.stringify(partidas, null, 2),
+      "utf8",
+    );
+  } catch (error) {
+    console.error("Error al guardar el archivo:", error);
+  }
+}
+
+// Buscamos partidas por si id
+function buscarPartidaPorId(id) {
+  const partidas = leerArchivo();
+  return partidas.find((partida) => partida.id === id);
+}
+
+// Agregar una nueva partida al archivo JSON
+function agregarPartida(partida) {
+  const partidas = leerArchivo();
+  partidas.push(partida);
+  guardarArchivo(partidas);
+  return partida;
+}
+
+// Actualizamos alguna partida
+function actualizarPartida(id, nuevaPartida) {
+  const partidas = leerArchivo();
+  const indice = partidas.findIndex((partida) => partida.id === id);
+
+  if (indice === -1) {
+    return null;
+  }
+
+  partidas[indice] = { ...partidas[indice], ...nuevaPartida };
+  guardarArchivo(partidas);
+  return partidas[indice];
+}
+
+module.exports = {
+  leerArchivo,
+  guardarArchivo,
+  buscarPartidaPorId,
+  agregarPartida,
+  actualizarPartida,
+};
