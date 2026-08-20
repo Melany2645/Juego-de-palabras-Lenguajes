bf0a202a48075988f1329805da01e01f4bb7ecd3
Melany Jirón Díaz <m.jiron.2@estudiantec.cr>
2026-08-20 13:08:08 -0600

    Implementación de React

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from "./pages/Inicio";
+import Juego from "./pages/Juego";
+import Historial from "./pages/Historial";
+import "./indez.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/juego/:id" element={<Juego />} />
+        <Route path="/historial" element={<Historial />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
